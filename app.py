4a8bcd00b66791cf0ee61e7d12f44aa59fe95229
Ben Smith <benSmith1981@users.noreply.github.com>
2025-12-17 11:04:17 +0000

    Update app.py

--- app.py
+++ app.py
@@ -387,6 +387,6 @@ RULES:
 # ===============================
 if __name__ == "__main__":
     #test only
-    app.run(debug=True)
-    # port = int(os.environ.get('PORT', 5000))
-    # app.run(host='0.0.0.0', port=port)
+    # app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
